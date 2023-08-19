b0febda2d11f6b01582cc779391b5660d839ff2c
gureum <gureum@gureumui-MacBookAir.local>
2023-08-19 17:24:46 +0900

    Init: 초기 폴더 설정

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    trailingComma: 'all',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'always',
+    htmlWhitespaceSensitivity: 'css',
+    singleAttributePerLine: false,
+};
