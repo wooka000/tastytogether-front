6936a7318eed3be13a2e8a16d85d5c4ab6061e44
[SW5] 김진규 <rlawlsrb16736@gmail.com>
2023-08-22 11:52:22 +0900

    Feat: 배너 이미지 추가 + App.js 헤더 설정 + 메인페이지용 헤더 추가 + 메인페이지에 배너 태그 추가

---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export default function MainHeader() {
+    return (
+        <Container>
+            <Link to="/">
+                <Img src="/imgs/logo1.png" alt="logo" />
+            </Link>
+            <Btns>
+                <Menu>
+                    <Link to="/post">
+                        <MenuBtn>혼밥 메이트</MenuBtn>
+                    </Link>
+                    <Link to="/stores/register">
+                        <MenuBtn>음식점 등록</MenuBtn>
+                    </Link>
+                </Menu>
+                <Link to="/users/login">
+                    <Login>로그인</Login>
+                </Link>
+            </Btns>
+        </Container>
+    );
+}
+
+const Container = styled.header`
+    z-index: 1;
+    position: fixed;
+    top: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 100px;
+    padding: 0px 25px;
+    border-bottom: 1px solid transparent;
+    background-color: rgba(255, 255, 255, 0.2);
+`;
+
+const Img = styled.img`
+    margin-right: 25px;
+`;
+
+const Menu = styled.nav`
+    flex-shrink: 0;
+`;
+
+const Btns = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const MenuBtn = styled.button`
+    color: white;
+    font-weight: bold;
+    background-color: transparent;
+    border: none;
+    font-size: 20px;
+    margin-right: 30px;
+
+    &:hover {
+        color: var(--color-accent);
+    }
+`;
+
+const Login = styled.button`
+    margin-left: 138px;
+    border: 2px solid var(--color-accent);
+    background-color: transparent;
+    font-size: 20px;
+    font-weight: bold;
+    color: var(--color-accent);
+    padding: 8px 20px;
+    border-radius: 30px;
+    min-width: 100px;
+    transition: all 250ms ease-out;
+
+    &:hover {
+        background-color: var(--color-accent);
+        color: white;
+    }
+`;
