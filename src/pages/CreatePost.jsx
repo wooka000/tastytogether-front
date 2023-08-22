239a046db0feb387af0b393a1c315ac5a73d66bd
[SW5] 김진규 <rlawlsrb16736@gmail.com>
2023-08-22 11:32:48 +0900

    Style: 모든 페이지 height 속성 -> min-height로 수정

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -6,6 +6,6 @@ export default function CreatePost() {
 }
 
 const Container = styled.div`
-    height: 100vh; // 페이지 높이를 100vh로 설정하여 스크롤을 내려야 footer가 보이게 설정
+    min-height: 100vh; // 페이지 높이를 100vh로 설정하여 스크롤을 내려야 footer가 보이게 설정
     margin-top: 100px; // 헤더의 포지션이 fixed여서 margin-top 값을 Header 높이 만큼 설정
 `;
