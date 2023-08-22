d428eca3e8adabaf95f8a534af69c2782414c28f
[SW5] 김진규 <rlawlsrb16736@gmail.com>
2023-08-22 11:01:19 +0900

    Style: 모든 페이지에서 Contailer -> Container로 태그 이름 오타 수정

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 
 export default function NotFound() {
-    return <Contailer>Page NotFound😅</Contailer>;
+    return <Container>Page NotFound😅</Container>;
 }
 
-const Contailer = styled.div`
+const Container = styled.div`
     height: 100vh; // 페이지 높이를 100vh로 설정하여 스크롤을 내려야 footer가 보이게 설정
     margin-top: 100px; // 헤더의 포지션이 fixed여서 margin-top 값을 Header 높이 만큼 설정
 `;
