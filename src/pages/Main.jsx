06411f9d141ca8f7a938bb9b9684533afa9ec0ab
[SW5] 김진규 <rlawlsrb16736@gmail.com>
2023-08-22 19:28:20 +0900

    음식 사진 추가 + BoardAnime 컴포넌트 생성 및 기능 구현(미완)

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import Banner from '../components/Banner';
+import BoardAnime from '../components/BoardAnime';
 
 export default function Main() {
     return (
         <Container>
             <Banner />
-            <BoardTitle>
-                <Text>혼밥 메이트 게시판</Text>에 올라온 게시글
-            </BoardTitle>
+            <BoardContainer>
+                <BoardTitle>
+                    <Text>혼밥 메이트 게시판</Text>에 올라온 게시글
+                </BoardTitle>
+                <BoardAnime />
+            </BoardContainer>
         </Container>
     );
 }
@@ -18,11 +22,16 @@ const Container = styled.div`
     margin-top: 100px; // 헤더의 포지션이 fixed여서 margin-top 값을 Header 높이 만큼 설정
 `;
 
-const BoardTitle = styled.h3`
-    color: black;
+const BoardContainer = styled.div`
     transform: translateY(450px);
     text-align: center;
+    margin: 0 auto;
+    color: black;
+`;
+
+const BoardTitle = styled.h3`
     font-size: 24px;
+    margin-bottom: 30px;
 `;
 
 const Text = styled.span`
