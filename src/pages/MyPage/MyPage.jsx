ef572328fedb6f46f7933d1b38ceb2b5aed76dc2
[SW5] 김진규 <rlawlsrb16736@gmail.com>
2023-08-25 20:24:01 +0900

    Feat: 헤더에 마이페이지 버튼 추가

---
+++ src/pages/MyPage/MyPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function MyPage() {
+    return <Container>MyPage</Container>;
+}
+
+const Container = styled.div`
+    min-height: 100vh; // 페이지 높이를 100vh로 설정하여 스크롤을 내려야 footer가 보이게 설정
+    margin-top: 100px; // 헤더의 포지션이 fixed여서 margin-top 값을 Header 높이 만큼 설정
+`;
