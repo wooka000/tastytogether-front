e98382999d09498683db8a06b472a2ba21f6b8dc
[SW5] 김진규 <rlawlsrb16736@gmail.com>
2023-08-27 16:07:15 +0900

    Feat: 마이페이지 마우스 호버하면 자세한 정보 보여주기 + 메뉴탭 활성화 기능

--- src/components/MyPage/MyReview.jsx
+++ src/components/MyPage/MyReview.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { styled } from 'styled-components';
+import * as S from './style/MyReview.style';
 
 export default function MyReview() {
     const [reviews, setReviews] = useState();
@@ -14,60 +14,21 @@ export default function MyReview() {
         getData();
     }, []);
     return (
-        <TabBox>
+        <S.TabBox>
             {reviews &&
                 reviews.map((review) => {
                     return (
-                        <Box key={review.id}>
-                            <Img src={review.photo} />
-                        </Box>
+                        <S.Box key={review.id}>
+                            <S.Img src={review.photo} />
+                            <S.Text>
+                                <S.Date>{review.date}</S.Date>
+                                <S.Grade>⭐️{review.grade}</S.Grade>
+                                <S.Title>{review.title}</S.Title>
+                                <S.Content>{review.content}</S.Content>
+                            </S.Text>
+                        </S.Box>
                     );
                 })}
-        </TabBox>
+        </S.TabBox>
     );
 }
-
-const TabBox = styled.div`
-    margin: 0px auto;
-    margin-top: 160px;
-    max-width: 1200px;
-    height: 400px;
-    overflow-y: auto;
-    overflow-x: hidden;
-    padding: 0 40px 35px 40px;
-    background-color: white;
-    border-bottom-left-radius: 10px;
-    border-bottom-right-radius: 10px;
-    position: relative;
-    display: grid;
-    grid-template-columns: repeat(6, 180px);
-    grid-template-rows: auto;
-    gap: 5px;
-    justify-content: center;
-
-    &::-webkit-scrollbar {
-        width: 8px; /* 스크롤바 너비 설정 */
-    }
-
-    &::-webkit-scrollbar-thumb {
-        background-color: var(--color-accent); /* 스크롤바 색상 설정 */
-        border-radius: 4px; /* 스크롤바 모서리 둥글기 설정 */
-    }
-
-    &::-webkit-scrollbar-track {
-        background-color: lightgray; /* 스크롤바 트랙 색상 설정 */
-    }
-`;
-
-const Box = styled.div`
-    border: 1px solid gray;
-    border-radius: 10px;
-    overflow: hidden;
-    cursor: pointer;
-    height: 180px;
-`;
-
-const Img = styled.img`
-    width: 100%;
-    height: 100%;
-`;
--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import * as S from './style/MyPage.style';
 import MyBoard from '../../components/MyPage/MyBoard';
 import MyReview from '../../components/MyPage/MyReview';
 import MyBookmark from '../../components/MyPage/MyBookmark';
@@ -22,168 +22,45 @@ export default function MyPage() {
         console.log(bgFile);
     };
     return (
-        <Container>
+        <S.Container>
             {modalOpen && <MyProfileEdit setModalOpen={setModalOpen}></MyProfileEdit>}
-            <ProfileBg file={bgFile ? bgFile : ''}>
-                <ShowInputFile htmlFor="file">
+            <S.ProfileBg file={bgFile ? bgFile : ''}>
+                <S.ShowInputFile htmlFor="file">
                     <BiImageAdd />
-                    <FileText>배경 사진 변경</FileText>
-                </ShowInputFile>
-                <InputFile
+                    <S.FileText>배경 사진 변경</S.FileText>
+                </S.ShowInputFile>
+                <S.InputFile
                     type="file"
                     name="file"
                     id="file"
                     accept="image/*"
                     onChange={handleChange}
                 />
-            </ProfileBg>
-            <ProfileBox>
-                <EditBtn onClick={() => setModalOpen(true)}>프로필 수정</EditBtn>
-                <Info>
-                    <Img src="/imgs/profile.png" alt="profile" />
-                    <Text>
-                        <Name>김망고</Name>
-                        <Description>안녕하세요 정직한 리뷰만 달고 있습니다~</Description>
-                    </Text>
-                </Info>
-                <Menu>
+            </S.ProfileBg>
+            <S.ProfileBox>
+                <S.EditBtn onClick={() => setModalOpen(true)}>프로필 수정</S.EditBtn>
+                <S.Info>
+                    <S.Img src="/imgs/profile.png" alt="profile" />
+                    <S.Text>
+                        <S.Name>김망고</S.Name>
+                        <S.Description>안녕하세요 정직한 리뷰만 달고 있습니다~</S.Description>
+                    </S.Text>
+                </S.Info>
+                <S.Menu>
                     {tabList.map((item, index) => {
                         return (
-                            <MenuBtn key={index} onClick={() => setTab(item.category)}>
-                                <Category>{item.category}</Category>
-                            </MenuBtn>
+                            <S.MenuBtn
+                                key={index}
+                                onClick={() => setTab(item.category)}
+                                active={tab === item.category ? 'active' : ''}
+                            >
+                                <S.Category>{item.category}</S.Category>
+                            </S.MenuBtn>
                         );
                     })}
-                </Menu>
-            </ProfileBox>
+                </S.Menu>
+            </S.ProfileBox>
             {tabComponent[tab]}
-        </Container>
+        </S.Container>
     );
 }
-
-const Container = styled.div`
-    min-height: 100vh; // 페이지 높이를 100vh로 설정하여 스크롤을 내려야 footer가 보이게 설정
-    margin-top: 100px; // 헤더의 포지션이 fixed여서 margin-top 값을 Header 높이 만큼 설정
-`;
-
-const ProfileBg = styled.div`
-    background-image: ${(props) =>
-        props.file === ''
-            ? 'url("/imgs/profilebg.jpg")'
-            : `url(${URL.createObjectURL(props.file)})`};
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    width: 100%;
-    height: 300px;
-`;
-
-const ShowInputFile = styled.label`
-    font-size: 20px;
-    color: gray;
-    float: right;
-    display: flex;
-    align-items: center;
-    margin: 10px;
-    border: 1px dotted gray;
-    border-radius: 10px;
-    padding: 5px 15px;
-    cursor: pointer;
-    transition: all 250ms ease-out;
-    &:hover {
-        color: black;
-        border: 1px solid black;
-    }
-`;
-
-const FileText = styled.span`
-    font-size: 14px;
-`;
-
-const InputFile = styled.input`
-    display: none;
-`;
-
-const ProfileBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    max-width: 1200px;
-    background-color: rgba(255, 255, 255, 0.8);
-    margin: -160px auto;
-    border-top-left-radius: 10px;
-    border-top-right-radius: 10px;
-    padding: 10px 40px;
-    position: relative;
-`;
-
-const EditBtn = styled.button`
-    width: 100px;
-    height: 30px;
-    background-color: transparent;
-    border-radius: 10px;
-    color: gray;
-    border: 1px dotted gray;
-    transition: all 250ms ease-out;
-    position: absolute;
-    top: 20px;
-    right: 20px;
-    &:hover {
-        color: black;
-        border: 1px solid black;
-    }
-`;
-
-const Info = styled.div`
-    display: flex;
-    align-items: center;
-`;
-
-const Img = styled.img`
-    width: 150px;
-    height: 150px;
-`;
-
-const Text = styled.div`
-    max-width: 800px;
-`;
-
-const Name = styled.p`
-    font-weight: bold;
-    font-size: 22px;
-`;
-
-const Description = styled.p`
-    font-size: 18px;
-    height: 100px;
-    padding-top: 10px;
-`;
-
-const Menu = styled.ul`
-    display: flex;
-    align-items: center;
-`;
-
-const MenuBtn = styled.li`
-    font-size: 20px;
-    font-weight: bold;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    border: 1px solid gray;
-    width: 150px;
-    margin: 10px 25px 0 25px;
-    border: none;
-    cursor: pointer;
-    color: grey;
-
-    &:hover {
-        p {
-            color: var(--color-accent);
-            transition: all 250ms ease-out;
-        }
-    }
-`;
-
-const Category = styled.p`
-    margin-bottom: 10px;
-`;
