fa5d61b13913c697cf2ca8a7a708c031d9308db5
[SW5] 김진규 <rlawlsrb16736@gmail.com>
2023-08-30 18:27:57 +0900

    Style: 검색결과페이지 Search + Nav + ResultDiv 스타일링 수정

--- src/pages/SearchResult/style/SearchResult.style.jsx
+++ src/pages/SearchResult/style/SearchResult.style.jsx
@@ -3,54 +3,37 @@ import styled from 'styled-components';
 export const Container = styled.div`
     min-height: 100vh; // 페이지 높이를 100vh로 설정하여 스크롤을 내려야 footer가 보이게 설정
     margin-top: 100px; // 헤더의 포지션이 fixed여서 margin-top 값을 Header 높이 만큼 설정
- `;
+`;
 
 export const Search = styled.div`
     width: 1440px;
-    min-height: 1024px;
-    display: flex;
-    flex-direction: row;
-    align-items: flex-start
-    justify-content: center;
-    background-color: #F8F9FA;
-    padding: 37px;
-    box-sizing: border-box;
-    top: 101px;
-    width: 1440px;
-    padding: 17px;
-    margin: auto;
+    height: auto;
+    background-color: transparent;
+    margin: 140px auto 40px;
     border-radius: 10px;
-    height: auto;
-    overflow: auto;
-`
+    position: relative;
+`;
 export const Nav = styled.div`
-    width: 324px;
-    height: 84%;
-    top: 120px;
-    max-height: 1300px;
-    height: auto;
-    background-color: #fff;
-    padding: 20px 20px 20px 20px;
-    margin-left: 10px;
-    margin-right: 41px;
+    width: 320px;
+    height: 600px;
+    background-color: white;
+    padding: 20px;
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
-    align-items: flex-start;
     position: fixed;
-    bottom: 120px;
+    top: 140px;
+    left: 70px;
     border-radius: 10px;
-`
+`;
 
 export const ResultDiv = styled.div`
-    width: 1030px;
-    min-height: 1000px;
-    background-color: #fff;
+    position: absolute;
+    top: 0;
+    left: 360px;
+    background-color: white;
     padding: 20px;
-    position: relative;
     border-radius: 10px;
-    left: 365px;
-`
+`;
 
 export const MyFilterTitle = styled.div`
     font-size: 1.3rem;
@@ -58,7 +41,7 @@ export const MyFilterTitle = styled.div`
     flex-direction: row;
     align-items: center;
     margin-bottom: 20px;
-`
+`;
 export const Square = styled.div`
     width: 25px;
     height: 25px;
@@ -68,35 +51,33 @@ export const Square = styled.div`
     border-radius: 100%;
     &:hover {
         background-color: var(--color-accent);
-        color: #f0f0f0;;
+        color: #f0f0f0;
     }
-
-`
+`;
 export const MyFilterContent = styled.div`
-    div{
+    div {
         display: flex;
         flex-direction: row;
         align-items: center;
         margin-bottom: 10px;
-        &:hover{
+        &:hover {
             cursor: pointer;
         }
-        img{
+        img {
             width: 20px;
             margin-right: 10px;
         }
-        h3{
+        h3 {
             font-size: 1.1rem;
-            &:hover{
+            &:hover {
                 background-color: var(--color-accent);
                 color: white;
                 border-radius: 3px;
             }
         }
     }
-`
+`;
 
-
-export const ResultStores = styled.div``
-export const Pagination = styled.div``
-export const Result = styled.div``
+export const ResultStores = styled.div``;
+export const Pagination = styled.div``;
+export const Result = styled.div``;
