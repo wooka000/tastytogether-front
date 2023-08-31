d89fee4aaaf244414911615df5184a4afad37f84
[SW5] 김진규 <rlawlsrb16736@gmail.com>
2023-08-31 17:17:31 +0900

    Feat: axios.js 파일 + useAxios.js 파일에 개발용 서버, 배포용 서버 첨부 필요 시 주석 처리하여 사용

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'http://34.22.80.41/api';
+const BASE_URL = 'http://localhost:8080'; // 개발용 서버
+// const BASE_URL = 'http://34.22.80.41/api'; // 배포용 서버
 
 export default axios.create({
     baseURL: BASE_URL,
