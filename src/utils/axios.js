469d42a999f330129c5ee3e61bfdae7239000335
Kimsuyeon <songudeum@gmail.com>
2023-09-02 05:49:22 +0900

    Refactor: 가게 등록 업종 모달 아이콘 변경 및 배포용 주소 설정

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:8080'; // 개발용 서버
-// const BASE_URL = 'http://34.22.80.41/api'; // 배포용 서버
+const BASE_URL = 'http://34.22.80.41/api';
 
 export default axios.create({
     baseURL: BASE_URL,
